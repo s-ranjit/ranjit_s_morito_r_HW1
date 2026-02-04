be1f1c86ac4b42ab7915e7172ee193224df01e02
Situ Ranjit <situranjit6@gmail.com>
2026-02-03 21:27:32 -0500

    add gsap
    add gsap

--- js/main.js
+++ js/main.js
@@ -98,4 +98,80 @@
     }
 
     backToTopButton.addEventListener("click", scrollToTop);
+
+
+
+    //  Header GSAP animations
+gsap.registerPlugin(ScrollTrigger); 
+gsap.from("#logo", {
+  opacity: 0,
+  y: -40,
+  duration: 2,
+  ease: "power3.out",
+  scrollTrigger: {
+    trigger: "#logo",
+    toggleActions: "play none none none",
+    start: "top 85%",
+    end: "bottom top"
+  }
+});
+gsap.from("#ham-burger", {
+  opacity: 0,
+  y: -40,
+  duration: 2,
+  ease: "power3.out",
+  scrollTrigger: {
+    trigger: "#logo",
+    toggleActions: "play none none none",
+    start: "top 85%",
+    end: "bottom top"
+  }
+});
+gsap.from(".burger-con", {
+  opacity: 0,
+  y: -40,
+  duration: 2,
+  ease: "power3.out",
+  scrollTrigger: {
+    trigger: ".burger-con",
+    toggleActions: "play none none none",
+    start: "top 85%",
+    end: "bottom top"
+  }
+});
+
+ gsap.from(".title", {
+    opacity: 0, 
+    y: -50,   
+    duration: 2,
+    ease: "power2.out",
+    stagger: 0.1  
+  });
+
+ 
+  gsap.from(".info", {
+    opacity: 0,
+    y: 30, 
+    duration: 1.2,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".info",
+        toggleActions: "play none none none",
+        start: "top 85%",
+        end: "bottom top"
+    }
+  });
+
+gsap.from(".footer", {
+  opacity: 0,
+  y: 20,
+  duration: 2,
+  ease: "power3.out",
+  scrollTrigger: {
+    trigger: ".footer",
+    toggleActions: "restart pause reverse pause",
+    start: "top 85%",
+    end: "bottom top"
+  }
+});
 })();
